d1b0d048de1755565d91af1dc9c25101f29da4f8
Damnnnnnnn <yejianxin1116@gmail.com>
2024-01-16 12:07:21 +0800

    init : update config.md

--- stores/config.ts
+++ stores/config.ts
@@ -1,29 +1,29 @@
-import webConfig from "../stores/config.json";
+import webConfig from '../config/website.json'
 
-export const CONFIG_KEY_BASE = "base";
-export const CONFIG_KEY_SEO = "seo";
-export const CONFIG_KEY_HERDER = "header";
+export const CONFIG_KEY_BASE = 'base'
+export const CONFIG_KEY_SEO = 'seo'
+export const CONFIG_KEY_HERDER = 'header'
 
-export const CONFIG_KEY_PENDANT = "pendant";
-export const CONFIG_KEY_FOOTER = "footer";
+export const CONFIG_KEY_PENDANT = 'pendant'
+export const CONFIG_KEY_FOOTER = 'footer'
 
 const KeyMap = {
-  CONFIG_KEY_BASE: "base",
-  CONFIG_KEY_SEO: "seo",
-  CONFIG_KEY_HERDER: "header",
-  CONFIG_KEY_PENDANT: "pendant",
-  CONFIG_KEY_FOOTER: "footer",
-};
+	CONFIG_KEY_BASE: 'base',
+	CONFIG_KEY_SEO: 'seo',
+	CONFIG_KEY_HERDER: 'header',
+	CONFIG_KEY_PENDANT: 'pendant',
+	CONFIG_KEY_FOOTER: 'footer',
+}
 
 // 获取配置信息
-export const getConfigItem = (key: string = "") => {
-  if (!key) {
-    return webConfig;
-  }
+export const getConfigItem = (key: string = '') => {
+	if (!key) {
+		return webConfig
+	}
 
-  if (!Object.values(KeyMap).includes(key)) {
-    return createError("无法获取配置信息");
-  }
+	if (!Object.values(KeyMap).includes(key)) {
+		return createError('无法获取配置信息')
+	}
 
-  return webConfig[key];
-};
+	return webConfig[key]
+}
