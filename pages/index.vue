ec604b27b79a7f749b9a4b27812e3dbaba1a24ff
laosji <71160766+laosji@users.noreply.github.com>
2024-01-17 16:35:43 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -10,4 +10,6 @@
 <script setup lang="ts">
 import navList from '../config/nav.json'
 </script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3281954914625230"
+     crossorigin="anonymous"></script>
 <style></style>
