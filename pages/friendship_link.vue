78dc4a6e7d5d7a8dcf294d70736b072e440bb5fe
linsk27 <160390087+linsk27@users.noreply.github.com>
2024-07-14 11:55:12 +0800

    feat: friendship_link_ui work

--- pages/friendship_link.vue
+++ pages/friendship_link.vue
@@ -1,120 +1,92 @@
 <template>
   <div class="body">
-    
-      <!-- 页面头部 --> 
-      <header>
-          <div class="header-box">
-              <div class="header-icon"><img :class="imgCss" @mouseenter="imgCss = 'header-icon-rotate-right'"
-                      @mouseleave="imgCss = 'header-icon-rotate-left'" src="/public/img/fre123_icon.png"
-                      width="85px" /><span>Fre123</span></div>
-              <div class="header-search" @mouseenter="inputColor = true" @mouseleave="inputColor = false">
-                  <div @click="waysAll = !waysAll" class="header-search-ways">
-                      <span class="waysAll">{{ wayType }}</span>
-                      <ul v-if="waysAll">
-                          <li v-for="item in typeData" @click="wayType = item">{{ item }}</li>
-                      </ul>
-                  </div>
-                  <input @focusin="inputClick = true" @focusout="inputClick = false" :class="inputCss" />
-                  <img src="/public/img/search.png" />
-              </div>
-              <div class="header-ads">
-                  <div class="header-ads-smallBox">
-                      <img src="/public/img/Eyes.png" />
-                      <text>动漫搜</text>
-                  </div>
-                  <div class="header-ads-smallBox">
-                      <img src="/public/img/right.png" />
-                      <text>怎么用</text>
-                  </div>
-                  <div class="header-ads-smallBox">
-                      <img src="/public/img/fire.png" />
-                      <text>资源群</text>
-                  </div>
-              </div>
-              <div class="ads-icon">
-                  <img src="/public/img/ad.png" @click="adClick = !adClick" />
-                  <ul v-if="adClick">
-                      <li>
-                          <img src="/public/img/Eyes.png" />
-                          <text>动漫搜</text>
-                      </li>
-                      <li>
-                          <img src="/public/img/right.png" />
-                          <text>怎么用</text>
-                      </li>
-                      <li>
-                          <img src="/public/img/fire.png" />
-                          <text>资源群</text>
-                      </li>
-                  </ul>
-              </div>
+
+    <!-- 页面头部 -->
+    <header>
+      <div class="header-box">
+        <div class="header-icon"><img :class="imgCss" @mouseenter="imgCss = 'header-icon-rotate-right'"
+            @mouseleave="imgCss = 'header-icon-rotate-left'" src="/public/img/fre123_icon.png"
+            width="85px" /><span>Fre123</span></div>
+        <div class="header-search" @mouseenter="inputColor = true" @mouseleave="inputColor = false">
+          <div @click="waysAll = !waysAll" class="header-search-ways">
+            <span class="waysAll">{{ wayType }}</span>
+            <ul v-if="waysAll">
+              <li v-for="item in typeData" @click="wayType = item.name">{{ item.name }}</li>
+            </ul>
           </div>
-      </header>
-      <!-- 页面主要内容 -->
-      <main>
-          <div class="main-content">
-              <div class="main-kinds-box">
-                  <div class="kinds-box-top"><strong>网盘资源</strong></div>
-                  <div class="kinds-box-bottom">
-                      <div class="box-bottom-box">云际寻宝</div>
-                      <div class="box-bottom-box">之乐搜</div>
-                      <div class="box-bottom-box">撒大撒大撒大撒</div>
-                      <div class="box-bottom-box">测试grid第二行</div>
-                  </div>
-              </div>
-              <div class="main-kinds-box">
-                  <div class="kinds-box-top"><strong>导航资源</strong></div>
-                  <div class="kinds-box-bottom">
-                      <div class="box-bottom-box">FRE123免费资源导航</div>
-                      <div class="box-bottom-box">云际寻宝</div>
-                  </div>
-              </div>
-              <div class="main-kinds-box">
-                  <div class="kinds-box-top"><strong>导航资源</strong></div>
-                  <div class="kinds-box-bottom">
-                      <div class="box-bottom-box">FRE123免费资源导航</div>
-                      <div class="box-bottom-box">云际寻宝</div>
-                  </div>
-              </div>
-              <div class="main-kinds-box">
-                  <div class="kinds-box-top"><strong>导航资源</strong></div>
-                  <div class="kinds-box-bottom">
-                      <div class="box-bottom-box">FRE123免费资源导航</div>
-                      <div class="box-bottom-box">云际寻宝</div>
-                  </div>
-              </div>
-              <div class="main-kinds-box">
-                  <div class="kinds-box-top"><strong>导航资源</strong></div>
-                  <div class="kinds-box-bottom">
-                      <div class="box-bottom-box">FRE123免费资源导航</div>
-                      <div class="box-bottom-box">云际寻宝</div>
-                  </div>
-              </div>
+          <input @focusin="inputClick = true" @focusout="inputClick = false" :class="inputCss" />
+          <img src="/public/img/search.png" />
+        </div>
+        <div class="header-ads">
+          <div class="header-ads-smallBox">
+            <img src="/public/img/Eyes.png" />
+            <text>动漫搜</text>
           </div>
-
-
-      </main>
-
-      <div class="move-people"></div>
-      <div class="body-fre123"></div>
-
-      <!-- 页面底部 -->
-      <footer>
-          <div class="footer-box">
-              <div class="footer-top">
-                  <span><strong>友情链接：</strong></span>
-                  <ul>
-                      <li><a>云迹寻宝</a></li>
-                      <li><a>知了搜</a></li>
-                      <li><a>FRE123 免费资源导航</a></li>
-                      <li><a><span>查看更多>>></span></a></li>
-                  </ul>
-              </div>
-              <div class="footer-bottom">
-                  <text>声明：本站内容由网友分享提供。本站不储存、复制、传播任何文件，仅作个人公益学习，请勿非法传播，如有侵权，请联系 1809578063@qq.com 删除</text>
-              </div>
+          <div class="header-ads-smallBox">
+            <img src="/public/img/right.png" />
+            <text>怎么用</text>
           </div>
-      </footer>
+          <div class="header-ads-smallBox">
+            <img src="/public/img/fire.png" />
+            <text>资源群</text>
+          </div>
+        </div>
+        <div class="ads-icon">
+          <img src="/public/img/ad.png" @click="adClick = !adClick" />
+          <ul v-if="adClick">
+            <li>
+              <img src="/public/img/Eyes.png" />
+              <text>动漫搜</text>
+            </li>
+            <li>
+              <img src="/public/img/right.png" />
+              <text>怎么用</text>
+            </li>
+            <li>
+              <img src="/public/img/fire.png" />
+              <text>资源群</text>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+
+    <!-- 页面主要内容 -->
+    <main>
+      <div class="main-content">
+        <div class="main-kinds-box" v-for="(item,index) in  typeData" :key="index">
+          <div class="kinds-box-top" v-if="item.name!=='全部'"><strong>{{ item.name }}资源</strong></div>
+          <div class="no-link-data" v-if="item.name!=='全部'&&showLink[item.name].length===0">
+            <div>暂无资源</div>
+          </div>
+          <div class="kinds-box-bottom" v-if="item.name!=='全部'&&showLink[item.name].length!==0">
+            <div class="box-bottom-box" v-for="link in showLink[item.name]" @click="goLink(link.url)"><img :src="link.url" />{{ link.name}}</div>
+          </div>
+        </div>
+      </div>
+
+    </main>
+
+    <div class="move-people"></div>
+    <div class="body-fre123"></div>
+
+    <!-- 页面底部 -->
+    <footer>
+      <div class="footer-box">
+        <div class="footer-top">
+          <span><strong>友情链接：</strong></span>
+          <ul>
+            <li><a>云迹寻宝</a></li>
+            <li><a>知了搜</a></li>
+            <li><a>FRE123 免费资源导航</a></li>
+            <li><a href=""><span>查看更多>>></span></a></li>
+          </ul>
+        </div>
+        <div class="footer-bottom">
+          <text>声明：本站内容由网友分享提供。本站不储存、复制、传播任何文件，仅作个人公益学习，请勿非法传播，如有侵权，请联系 1809578063@qq.com 删除</text>
+        </div>
+      </div>
+    </footer>
 
   </div>
 </template>
@@ -124,6 +96,80 @@ const imgCss = ref('')
 
 definePageMeta({
   layout: false  // 这行代码告诉 Nuxt 不使用布局
+})
+
+const typeData = ref([{ name: '全部' }])
+const linkData = ref<any>([])
+const showLink = ref<any>({})
+
+// 通过本地获取数据
+const getData=()=>{
+  const data = localStorage.getItem('friend_data');
+  if(data){
+    try{
+      linkData.value = JSON.parse(data).data;
+      // types字段赋值
+  for (let i = 0; i < linkData.value.types.length; i++) {
+        if (linkData.value.types[i].count !== 0) {
+          typeData.value = typeData.value.concat(linkData.value.types[i])
+          showLink.value[linkData.value.types[i].name]=[]
+        }
+      }
+      // 获取键名进行类别判断赋值
+      let keys = Object.keys(showLink.value);
+      // 
+      for(let i=0;i<linkData.value.rows.length;i++){
+        for(let j =0;j<keys.length;j++){
+          if(linkData.value.rows[i].type===keys[j]&&linkData.value.rows[i].status===true){
+            showLink.value[keys[j]].push(linkData.value.rows[i])
+          }
+        }
+      }
+      console.log("showLink:",showLink.value);
+    }catch{
+      console.log("JSON数据解析错误");
+    }
+  }else{
+    showLink.value=[];
+  }
+}
+
+onMounted(async () => { 
+  getData();
+  // 通过fetch获取数据
+  // await fetch('https://raw.githubusercontent.com/linsk27/amz123jsonData/main/friendship_link')
+  //   .then(response => {
+  //     return response.json();
+  //   })
+  //   .then(data => {
+
+  //     linkData.value = data.data
+
+  //     for (let i = 0; i < linkData.value.types.length; i++) {
+  //       if (linkData.value.types[i].count !== 0) {
+  //         typeData.value = typeData.value.concat(linkData.value.types[i])
+  //         showLink.value[linkData.value.types[i].name]=[]
+  //       }
+  //     }
+
+  //     // 获取键名进行类别判断赋值
+  //     let keys = Object.keys(showLink.value);
+  //     for(let i=0;i<linkData.value.rows.length;i++){
+  //       for(let j =0;j<keys.length;j++){
+  //         if(linkData.value.rows[i].type===keys[j]){
+  //           showLink.value[keys[j]].push(linkData.value.rows[i])
+  //         }
+  //       }
+  //     } 
+  //     console.log("showLink:",showLink.value);
+      
+  //   })
+  //   .catch(error => {
+  //     console.log("get error:", error);
+  //   })
+  //   .finally(() => {
+  //     console.log("fetch over");
+  //   })
 })
 
 // 输入框聚焦判断css变化板块
@@ -136,13 +182,17 @@ const adClick = ref(false)
 // 类型选择
 const wayType = ref('全部')
 
+// 输入框样式变化
 const inputCss = computed(() => {
   if (inputColor.value === true || inputClick.value === true) {
-      return "header-search-input"
+    return "header-search-input"
   }
 })
 
-const typeData = ref(['全部', '小说', '博客', '电子书', '导航', '网站', '视频', '音乐'])
+// 链接跳转
+const goLink =async(link: string | URL | undefined)=>{
+  window.open(link, "_blank");
+}
 
 </script>
 
@@ -153,7 +203,7 @@ li {
 
 
 .body {
-position: relative;
+  position: relative;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
@@ -168,7 +218,7 @@ position: relative;
   background-repeat: no-repeat;
   background-position: center;
   z-index: -99;
-  opacity: 0.05;
+  opacity: 0.03;
   height: 100%;
   width: 100%;
 }
@@ -384,13 +434,29 @@ main {
   padding: 10px
 }
 
+.no-link-data{
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: 800;
+  color: #5b718b;
+}
+
 .box-bottom-box {
+  display: flex;
+  align-items: center;
+  justify-content: center;
   cursor: pointer;
   text-align: center;
   width: 100%;
   border-radius: 10px;
   border: 1px solid #eff3f7;
   padding: 8px 0;
+}
+
+.box-bottom-box img{
+  width: 25px;
 }
 
 .box-bottom-box:hover {
@@ -446,104 +512,104 @@ footer {
   width: 100px;
   z-index: -1;
   animation:
-      run 2s steps(12) infinite
+    run 2s steps(12) infinite
 }
 
 @keyframes run {
   from {
-      background-position: 0 0;
+    background-position: 0 0;
   }
 
   to {
-      background-position: -1200px 0;
+    background-position: -1200px 0;
   }
 }
 
 /* 头部响应式 */
 @media(max-width: 1400px) {
   .header-search input {
-      width: 500px;
+    width: 500px;
   }
 
   .move-people {
-      display: none;
+    display: none;
   }
 }
 
 @media(max-width: 1100px) {
   .header-search input {
-      width: 350px;
+    width: 350px;
   }
 }
 
 @media(max-width: 1000px) {
   .header-box {
-      flex-direction: column;
-      padding-bottom: 22px;
+    flex-direction: column;
+    padding-bottom: 22px;
   }
 
   .header-search input {
-      width: 700px;
+    width: 700px;
   }
 
   .header-ads {
-      display: none;
+    display: none;
   }
 
   .ads-icon {
-      display: block;
+    display: block;
   }
 }
 
 @media(max-width: 800px) {
   .header-search input {
-      width: 500px;
+    width: 500px;
   }
 
   .main-kinds-box {
-      width: 600px;
+    width: 600px;
   }
 }
 
 @media(max-width: 600px) {
   .header-search input {
-      width: 350px;
+    width: 350px;
   }
 
   .main-kinds-box {
-      width: 400px;
+    width: 400px;
   }
 
   .footer-bottom {
-      display: none;
+    display: none;
   }
 }
 
 @media(max-width: 500px) {
   .header-search input {
-      width: 350px;
+    width: 350px;
   }
 
   .main-kinds-box {
-      width: 350px;
+    width: 350px;
   }
 
   .footer-box {
-      display: none;
+    display: none;
   }
 }
 
 @media(max-width: 400px) {
   .header-search input {
-      width: 250px;
+    width: 250px;
   }
 
   .main-kinds-box {
-      width: 300px;
+    width: 300px;
   }
 
   footer {
-      display: none;
+    display: none;
   }
 }
 </style>
