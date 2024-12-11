920b91f2149199e0b780640e01b3513ba93014d7
hoiii13 <1068637453@qq.com>
2024-12-11 11:00:26 +0800

    fix: links bugfix

--- fre123_nav_admin/src/views/friendship_link/components/TypeTable.vue
+++ fre123_nav_admin/src/views/friendship_link/components/TypeTable.vue
@@ -1,252 +1,290 @@
 <template>
-
-    <!-- 头部 -->
-    <div class="typeTable-top">
-        <div class="data-show-number">
-            <el-icon size="large" color="#409EFF" style="margin-right: 10px;">
-                <List />
-            </el-icon>类别数量：{{ props.tableList?.length }}
-        </div>
-        <div class="typeTable-top-refresh">
-            <el-button circle size="large" type="info" plain :icon="Refresh"
-                @click="friendshipLinkApi.refresh('刷新成功')"></el-button>
-        </div>
+  <!-- 头部 -->
+  <div class="typeTable-top">
+    <div class="data-show-number">
+      <el-icon size="large" color="#409EFF" style="margin-right: 10px">
+        <List /> </el-icon
+      >类别数量：{{ props.tableList?.length }}
     </div>
+    <div class="typeTable-top-refresh">
+      <el-button
+        circle
+        size="large"
+        type="info"
+        plain
+        :icon="Refresh"
+        @click="friendshipLinkApi.refresh('刷新成功')"
+      ></el-button>
+    </div>
+  </div>
 
-    <!-- 类别表格板块 -->
-    <!-- 使用 MetaTable 组件 -->
-    <MetaTable v-if="tableList?.length" @confirmDrag="updateNewSort" :isDraggable="true" :operateWidth="operateWidth" :key="metaTableKey" max-height="500"
-        :is-loading="props.loading" :tableData="props.tableList" :propList="props.propList"
-        @addData="dialogs[2].isVisible = true" @deleteData="deleteTypes" @editData="editGetKindOp"
-        @setTop="upTop">
-        <template #count="{ row }">
-            <text class="countColor" @click="typeToUrl(row.name)" :class="{ 'disabled': row.count === 0 }">{{ row.count
-                }}</text>
-        </template>
-        <template #created_at="{ row }">
-            <text>{{ friendshipLinkApi.formatUnixTimestamp(row.created_at) }}</text>
-        </template>
-        <template #status="{ row }">
-            <el-switch v-model="row.status" :loading="switchLoading" :active-value="1" :inactive-value="0"
-                @click="switchChange(row)"></el-switch>
-        </template>
-    </MetaTable>
+  <!-- 类别表格板块 -->
+  <!-- 使用 MetaTable 组件 -->
+  <MetaTable
+    @confirmDrag="updateNewSort"
+    :isDraggable="true"
+    :operateWidth="operateWidth"
+    :key="metaTableKey"
+    max-height="500"
+    :is-loading="props.loading"
+    :tableData="props.tableList ?? []"
+    :propList="props.propList"
+    @addData="dialogs[2].isVisible = true"
+    @deleteData="deleteTypes"
+    @editData="editGetKindOp"
+    @setTop="upTop"
+  >
+    <template #count="{ row }">
+      <text
+        class="countColor"
+        @click="typeToUrl(row.name)"
+        :class="{ disabled: row.count === 0 }"
+        >{{ row.count }}</text
+      >
+    </template>
+    <template #created_at="{ row }">
+      <text>{{ friendshipLinkApi.formatUnixTimestamp(row.created_at) }}</text>
+    </template>
+    <template #status="{ row }">
+      <el-switch
+        v-model="row.status"
+        :loading="switchLoading"
+        :active-value="1"
+        :inactive-value="0"
+        @click="switchChange(row)"
+      ></el-switch>
+    </template>
+  </MetaTable>
 
-    <!-- 使用 v-for 渲染多个 TypeDialog 组件 -->
-    <TypeDialog v-for="dialog in dialogs" :key="dialog.id" :id="dialog.id" :rules="opRules" :title="dialog.title"
-        :icon-type="dialog.iconType" :finishBtn="dialog.finishBtn" v-model="dialog.isVisible" :type-op="kindOp"
-        :kind-finish-dialog="kindFinishDialog" :clean-dialog="cleanDialog" />
-
+  <!-- 使用 v-for 渲染多个 TypeDialog 组件 -->
+  <TypeDialog
+    v-for="dialog in dialogs"
+    :key="dialog.id"
+    :id="dialog.id"
+    :rules="opRules"
+    :title="dialog.title"
+    :icon-type="dialog.iconType"
+    :finishBtn="dialog.finishBtn"
+    v-model="dialog.isVisible"
+    :type-op="kindOp"
+    :kind-finish-dialog="kindFinishDialog"
+    :clean-dialog="cleanDialog"
+  />
 </template>
 
 <script lang="ts" setup>
-import TypeDialog from "@/views/friendship_link/components/TypeDialog.vue";
-import { friendshipLinkApi, metaTableKey, tabName, tabActiveName, activeName, opForm, typeDataShow } from '@/views/friendship_link'
-import { opRules, dialogs } from "@/views/friendship_link/config/friendshipTop"
-import { MetaTable } from '../../../components';
-import { Refresh } from "@element-plus/icons-vue";
-import { ref } from "vue";
-import friendShipApi from "@/api/modules/friendship_link";
-import { infoMessage, successMessage } from "@/utils/notifications";
+import TypeDialog from '@/views/friendship_link/components/TypeDialog.vue'
+import {
+  friendshipLinkApi,
+  metaTableKey,
+  tabName,
+  tabActiveName,
+  activeName,
+  opForm,
+  typeDataShow,
+} from '@/views/friendship_link'
+import { opRules, dialogs } from '@/views/friendship_link/config/friendshipTop'
+import { MetaTable } from '../../../components'
+import { Refresh } from '@element-plus/icons-vue'
+import { ref } from 'vue'
+import friendShipApi from '@/api/modules/friendship_link'
+import { infoMessage, successMessage } from '@/utils/notifications'
 
 const props = defineProps<{
-    tableList: any[]; // 使用数组类型注解，而不是 Array<any>  
-    loading: boolean;
-    propList: any[]
-}>();
+  tableList: any[] // 使用数组类型注解，而不是 Array<any>
+  loading: boolean
+  propList: any[]
+}>()
 
 /**
  * @name 网址置顶
  */
 const upTop = async (row: any) => {
-    try {
-        let upArr: any = []
-        typeDataShow.value.forEach((e: { name: any; }) => {
-            upArr.push(e.name)
-        });
-        upArr = upArr.filter((obj: any) => obj !== row.name)
-        upArr.unshift(row.name)
-        await friendShipApi.sortTypeUser({
-            sorted_names: upArr
-        });
-        await friendshipLinkApi.getApiData();
-        metaTableKey.value++;
-        successMessage('置顶成功');
-        changeDragStatus()
-    } catch (error) {
-        // 可以选择将错误信息传递给用户或记录日志  
-        infoMessage('置顶出错,已撤销');
-    }
-};
+  try {
+    let upArr: any = []
+    typeDataShow.value.forEach((e: { name: any }) => {
+      upArr.push(e.name)
+    })
+    upArr = upArr.filter((obj: any) => obj !== row.name)
+    upArr.unshift(row.name)
+    await friendShipApi.sortTypeUser({
+      sorted_names: upArr,
+    })
+    await friendshipLinkApi.getApiData()
+    metaTableKey.value++
+    successMessage('置顶成功')
+    changeDragStatus()
+  } catch (error) {
+    // 可以选择将错误信息传递给用户或记录日志
+    infoMessage('置顶出错,已撤销')
+  }
+}
 
 let sortFinish: any = []
 
 const operateWidth = ref(100)
 //点击保存数据按钮，对数据进行保存操作
-const updateNewSort = async (list:any) => {
-    try {
-        operateWidth.value = 100;
-        list.forEach((e: { name: any; }) => {
-            sortFinish.push(e.name)
-        });
-        await friendShipApi.sortTypeUser({
-            sorted_names: sortFinish
-        });
-        await friendshipLinkApi.refresh('保存成功')
-    } catch (error) {
-        infoMessage('拖拽出错,已撤销');
-        return
-    }
-
+const updateNewSort = async (list: any) => {
+  try {
+    operateWidth.value = 100
+    list.forEach((e: { name: any }) => {
+      sortFinish.push(e.name)
+    })
+    await friendShipApi.sortTypeUser({
+      sorted_names: sortFinish,
+    })
+    await friendshipLinkApi.refresh('保存成功')
+  } catch (error) {
+    infoMessage('拖拽出错,已撤销')
+    return
+  }
 }
 
 //重置拖拽数据
 const initDragGroup = async (ifRefresh: boolean) => {
-    operateWidth.value = 100;
-    if (ifRefresh === true) await friendshipLinkApi.getApiData();
-    if (ifRefresh === true) infoMessage('已撤销')
-    metaTableKey.value++
+  operateWidth.value = 100
+  if (ifRefresh === true) await friendshipLinkApi.getApiData()
+  if (ifRefresh === true) infoMessage('已撤销')
+  metaTableKey.value++
 }
 
 //修改拖拽锁定状态
 const changeDragStatus = () => {
-    sortFinish = []
-    operateWidth.value = 140;
+  sortFinish = []
+  operateWidth.value = 140
 }
 
 // switch开关点击
 const switchLoading = ref(false)
 const switchChange = async (row: any) => {
-    try {
-        switchLoading.value = true
-        await friendShipApi.updateUser({
-            _id: row._id,
-            name: row.name,
-            status: row.status,
-        })
-        await friendshipLinkApi.getApiData()
-        switchLoading.value = false
-    } catch {
-        console.log("status改变错误");
-    }
+  try {
+    switchLoading.value = true
+    await friendShipApi.updateUser({
+      _id: row._id,
+      name: row.name,
+      status: row.status,
+    })
+    await friendshipLinkApi.getApiData()
+    switchLoading.value = false
+  } catch {
+    console.log('status改变错误')
+  }
 }
 
 //类别操作数组
 const kindOp = ref<any>({
-    _id: '',
-    name: '',
-    status: -1,
+  _id: '',
+  name: '',
+  status: -1,
 })
 
 // 清空数据
 const cleanDialog = () => {
-    dialogs.value.forEach(e => {
-        e.isVisible = false
-    });
-    kindOp.value = {
-        _id: '',
-        name: '',
-        status: -1,
-    }
-    opForm.value.resetFields(); // 重置字段和验证状态
+  dialogs.value.forEach((e) => {
+    e.isVisible = false
+  })
+  kindOp.value = {
+    _id: '',
+    name: '',
+    status: -1,
+  }
+  opForm.value.resetFields() // 重置字段和验证状态
 }
 
 /**
  * @name 点击编辑获取该条类别数据
  */
 const editGetKindOp = (row: any) => {
-    dialogs.value.forEach(e => {
-        if (e.id === 'typeEdit') e.isVisible = true
-    });
-    kindOp.value = { ...row }
+  dialogs.value.forEach((e) => {
+    if (e.id === 'typeEdit') e.isVisible = true
+  })
+  kindOp.value = { ...row }
 }
 
 /**
  * @name 点击类别数量跳转到网址并且自动分类
  */
 const typeToUrl = (name: string) => {
-    activeName.value = 'web'
-    tabName.value = name
-    tabActiveName.value = name
-    friendshipLinkApi.handleTabClickUse()
+  activeName.value = 'web'
+  tabName.value = name
+  tabActiveName.value = name
+  friendshipLinkApi.handleTabClickUse()
 }
 
 /**
  * @name 弹窗确认按钮
  */
 const kindFinishDialog = async (id: string) => {
-    try {
-        await friendshipLinkApi.validateForm((valid) => {
-            if (valid) {
-                if (id === "add") {
-                    insertTypes()
-                } else {
-                    updateTypes()
-                }
-            } else {
-                console.log('表单验证失败');
-            }
-        });
-    } catch (error) {
-        console.log(error,111);
-    }
+  try {
+    await friendshipLinkApi.validateForm((valid) => {
+      if (valid) {
+        if (id === 'add') {
+          insertTypes()
+        } else {
+          updateTypes()
+        }
+      } else {
+        console.log('表单验证失败')
+      }
+    })
+  } catch (error) {
+    console.log(error, 111)
+  }
 }
 
 // type添加操作
 const insertTypes = async () => {
-    try {
-        console.log(kindOp.value.name, kindOp.value.status);
+  try {
+    console.log(kindOp.value.name, kindOp.value.status)
 
-        const data = await friendShipApi.insertUser({
-            name: kindOp.value.name,
-            status: kindOp.value.status,
-            list: []
-        })
-        friendshipLinkApi.refresh('添加成功');
-        cleanDialog();
-    } catch(error:any) {
-        infoMessage(error.err_msg);
-        
-    }
+    const data = await friendShipApi.insertUser({
+      name: kindOp.value.name,
+      status: kindOp.value.status,
+      list: [],
+    })
+    friendshipLinkApi.refresh('添加成功')
+    cleanDialog()
+  } catch (error: any) {
+    infoMessage(error.err_msg)
+  }
 }
 //type更新操作
 const updateTypes = async () => {
-    try {
-        await friendShipApi.updateUser({
-            _id: kindOp.value._id,
-            name: kindOp.value.name,
-            status: kindOp.value.status,
-        })
-        friendshipLinkApi.refresh('编辑成功');
-        cleanDialog();
-    } catch {
-        console.log("数据更新错误");
-    }
+  try {
+    await friendShipApi.updateUser({
+      _id: kindOp.value._id,
+      name: kindOp.value.name,
+      status: kindOp.value.status,
+    })
+    friendshipLinkApi.refresh('编辑成功')
+    cleanDialog()
+  } catch {
+    console.log('数据更新错误')
+  }
 }
 // type删除操作
 const deleteTypes = async (row: any) => {
-    try {
-        await friendShipApi.delUser({
-            ids: [row._id]
-        })
-        friendshipLinkApi.refresh("删除成功");
-    } catch {
-        console.log("数据删除错误");
-    }
+  try {
+    await friendShipApi.delUser({
+      ids: [row._id],
+    })
+    friendshipLinkApi.refresh('删除成功')
+  } catch {
+    console.log('数据删除错误')
+  }
 }
-
 </script>
 
 <style lang="css" scope>
 .countColor {
-    cursor: pointer;
-    color: #409EFF;
+  cursor: pointer;
+  color: #409eff;
 }
 
 .disabled {
-    pointer-events: none;
-    /* 阻止点击事件 */
-    color: #ccc;
-    /* 可选：改变颜色以表示禁用状态 */
+  pointer-events: none;
+  /* 阻止点击事件 */
+  color: #ccc;
+  /* 可选：改变颜色以表示禁用状态 */
 }
 </style>
--- fre123_nav_admin/src/views/nav_site/index.vue
+++ fre123_nav_admin/src/views/nav_site/index.vue
@@ -1,25 +1,59 @@
 <template>
   <div>
-    <el-tabs type="card" v-loading="!tableData || loading" style="display: block; width: 100%" @tab-click="TabClick">
-      <el-tab-pane v-for="(group, groupIndex) in tableData" :label="group.group_name" :key="groupIndex">
+    <el-tabs
+      type="card"
+      v-loading="!tableData || loading"
+      style="display: block; width: 100%"
+      @tab-click="TabClick"
+    >
+      <el-tab-pane
+        v-for="(group, groupIndex) in tableData"
+        :label="group.group_name"
+        :key="groupIndex"
+      >
         <template #default>
-          <el-tabs v-model:activeName="tabName" tab-position="left" class="demo-tabs"
-            style="display: block; width: 100%" @tab-click="handleTabClick">
+          <el-tabs
+            v-model:activeName="tabName"
+            tab-position="left"
+            class="demo-tabs"
+            style="display: block; width: 100%"
+            @tab-click="handleTabClick"
+          >
             <div v-if="!group.tab_list || group.tab_list.length === 0">
               当前没有数据,请返回分类管理添加
             </div>
-            <el-tab-pane v-for="(tab, tabIndex) in group.tab_list" :label="tab.tab_name" :key="tabIndex"
-              :class="tab.tab_name">
-              <MetaTable v-if="tabName === tab.tab_name" :propList="tableConfig.propList" :tableData="tab.details"
-                @addData="newDialog(groupIndex, tabIndex)" @edit-data="showDialog($event, groupIndex, tabIndex)"
-                @delete-data="deleteRow($event, groupIndex, tabIndex)" :isDraggable=true
-                @confirm-drag="updateNewSort($event, groupIndex, tabIndex)" :key="metaTableKey">
+            <el-tab-pane
+              v-for="(tab, tabIndex) in group.tab_list"
+              :label="tab.tab_name"
+              :key="tabIndex"
+              :class="tab.tab_name"
+            >
+              <MetaTable
+                v-if="tabName === tab.tab_name"
+                :propList="tableConfig.propList ?? []"
+                :tableData="tab.details"
+                @addData="newDialog(groupIndex, tabIndex)"
+                @edit-data="showDialog($event, groupIndex, tabIndex)"
+                @delete-data="deleteRow($event, groupIndex, tabIndex)"
+                :isDraggable="true"
+                @confirm-drag="updateNewSort($event, groupIndex, tabIndex)"
+                :key="metaTableKey"
+              >
                 <template #icon="scope">
-                  <el-image style="width: 50px; height: 50px" :src="scope.row.icon" :fit="scope.row.fit" />
+                  <el-image
+                    style="width: 50px; height: 50px"
+                    :src="scope.row.icon"
+                    :fit="scope.row.fit"
+                  />
                 </template>
                 <template #is_show="scope">
-                  <el-switch v-model="scope.row.is_show" label="显示" :active-value="1" :inactive-value="0"
-                    @click="change(groupIndex, tabIndex)" />
+                  <el-switch
+                    v-model="scope.row.is_show"
+                    label="显示"
+                    :active-value="1"
+                    :inactive-value="0"
+                    @click="change(groupIndex, tabIndex)"
+                  />
                 </template>
               </MetaTable>
             </el-tab-pane>
@@ -29,51 +63,58 @@
     </el-tabs>
 
     <!-- 分割线---------下面是抽屉等等一系列东西------------------------------------------------------------------------ -->
-    <SiteDialog :visible="currentRow.drawerVisible" :form-data="formData" :dialog-header="tShow" :edit-mode="bShow"
-      @on-submit="onSubmit" @on-submit-new="newSubmit" @closed="currentRow.drawerVisible = false">
+    <SiteDialog
+      :visible="currentRow.drawerVisible"
+      :form-data="formData"
+      :dialog-header="tShow"
+      :edit-mode="bShow"
+      @on-submit="onSubmit"
+      @on-submit-new="newSubmit"
+      @closed="currentRow.drawerVisible = false"
+    >
     </SiteDialog>
   </div>
 </template>
 <script lang="ts" setup>
-import { IRequestBody } from "@/api/modules/nav/interface";
-import MetaTable from "@/components/MetaTable/index.vue";
-import { navStore } from "@/store/modules/nav";
-import { FormInstance } from "element-plus";
-import { nextTick, onMounted, ref } from "vue";
-import SiteDialog from "./components/SiteDialog.vue";
+import { IRequestBody } from '@/api/modules/nav/interface'
+import MetaTable from '@/components/MetaTable/index.vue'
+import { navStore } from '@/store/modules/nav'
+import { FormInstance } from 'element-plus'
+import { nextTick, onMounted, ref } from 'vue'
+import SiteDialog from './components/SiteDialog.vue'
 import tableConfig, {
   openSuccessEdit,
   openSuccessNew,
   openWaring,
   TabItem,
   updateNav,
-} from "./index";
+} from './index'
 
 //更新数据的获取
 // 当前激活的 group 和 tab 索引
-const metaTableKey = ref(0);
-const { refetchNav, navData } = navStore();
-const deepClone = (obj: any) => JSON.parse(JSON.stringify(obj));
-const loading = ref(false);
-const storedData = ref<TabItem>();
-const _id = ref("");
+const metaTableKey = ref(0)
+const { refetchNav, navData } = navStore()
+const deepClone = (obj: any) => JSON.parse(JSON.stringify(obj))
+const loading = ref(false)
+const storedData = ref<TabItem>()
+const _id = ref('')
 const requestBody = ref<IRequestBody>({
   _id: _id.value,
-  group_name: "",
-  style: "",
-  style_des: "",
+  group_name: '',
+  style: '',
+  style_des: '',
   tab_list: storedData.value,
-  group_name_url: "",
-});
-const tableData = ref("") as any;
-const bShow = ref(true);
-const tShow = ref("");
+  group_name_url: '',
+})
+const tableData = ref('') as any
+const bShow = ref(true)
+const tShow = ref('')
 const formData = ref({
-  title: "",
-  url: "",
-  description: "",
-  icon: "",
-});
+  title: '',
+  url: '',
+  description: '',
+  icon: '',
+})
 //
 
 // const dIndex = ref(0)
@@ -81,53 +122,52 @@ const formData = ref({
 const download = async () => {
   try {
     // 请求数据
-    await refetchNav; // 确保数据被请求
-    let isDataLoaded = false; // 数据加载标志
+    await refetchNav // 确保数据被请求
+    let isDataLoaded = false // 数据加载标志
     // 循环直到数据有效
     while (!isDataLoaded) {
-      await nextTick(); // 等待下一次 DOM 更新
+      await nextTick() // 等待下一次 DOM 更新
       // 检查数据格式
-      if (navData.value?.data && typeof navData.value.data === "object") {
+      if (navData.value?.data && typeof navData.value.data === 'object') {
         if (
-          "rows" in navData.value.data &&
+          'rows' in navData.value.data &&
           Array.isArray(navData.value.data.rows)
         ) {
-          loading.value = true;
-          tableData.value = deepClone(navData.value.data.rows);
-          loading.value = false;
-          isDataLoaded = true; // 数据已加载
+          loading.value = true
+          tableData.value = deepClone(navData.value.data.rows)
+          loading.value = false
+          isDataLoaded = true // 数据已加载
         } else {
-          tableData.value = [];
-          await new Promise((resolve) => setTimeout(resolve, 500)); // 延迟后重试
+          tableData.value = []
+          await new Promise((resolve) => setTimeout(resolve, 500)) // 延迟后重试
         }
       } else {
-        await new Promise((resolve) => setTimeout(resolve, 500)); // 延迟后重试
+        await new Promise((resolve) => setTimeout(resolve, 500)) // 延迟后重试
       }
     }
-  } catch (error) {
-  }
-};
+  } catch (error) {}
+}
 const reloadHeader = async () => {
-  await refetchNav.value();
-};
+  await refetchNav.value()
+}
 const updateHeader = async () => {
   try {
-    loading.value = true;
-    await updateData(); // 更新数据
-    await updateNav(requestBody.value); // 更新导航数据
-    loading.value = false;
-    await reloadHeader(); // 重新加载头部信息
+    loading.value = true
+    await updateData() // 更新数据
+    await updateNav(requestBody.value) // 更新导航数据
+    loading.value = false
+    await reloadHeader() // 重新加载头部信息
   } catch (error) {
-    await reloadHeader(); // 在更新失败时重新加载头部信息
+    await reloadHeader() // 在更新失败时重新加载头部信息
   }
-};
+}
 //更新数据
 const updateData = async () => {
-  const { groupIndex } = editingIndex.value || {};
+  const { groupIndex } = editingIndex.value || {}
   if (groupIndex === undefined || !tableData.value[groupIndex]) {
-    return;
+    return
   }
-  const groupData = tableData.value[groupIndex];
+  const groupData = tableData.value[groupIndex]
   requestBody.value = {
     ...requestBody.value, // 保留其他字段
     _id: groupData._id,
@@ -136,58 +176,59 @@ const updateData = async () => {
     style_des: groupData.style_des,
     group_name_url: groupData.group_name_url,
     tab_list: deepClone(groupData.tab_list), // 更新数据
-  };
-};
+  }
+}
 const getIndex = (row: any, groupIndex: number, tabIndex: number) => {
-  const rowIndex = tableData.value[groupIndex].tab_list[tabIndex].details.indexOf(row);
-  return rowIndex;
-};
+  const rowIndex =
+    tableData.value[groupIndex].tab_list[tabIndex].details.indexOf(row)
+  return rowIndex
+}
 
 // --------------------
 
 const editingIndex = ref<{
-  groupIndex: any;
-  tabIndex: any;
-  detailIndex: any;
-} | null>(null); //这是要修改的地方的索引
+  groupIndex: any
+  tabIndex: any
+  detailIndex: any
+} | null>(null) //这是要修改的地方的索引
 //定义数据用于input里面的修改
-const currentRow = ref({ drawerVisible: false });
+const currentRow = ref({ drawerVisible: false })
 //获取对应的index
 const tabName = ref('')
 function handleTabClick(activeName: any) {
   tabName.value = activeName.props.label
-  metaTableKey.value++; // Trigger re-render if necessary
+  metaTableKey.value++ // Trigger re-render if necessary
 }
 function TabClick(activeName: any) {
-  const index = activeName.index;
-  tabName.value = tableData.value[index].tab_list[0].tab_name;
-  metaTableKey.value++; // Trigger re-render if necessary
+  const index = activeName.index
+  tabName.value = tableData.value[index].tab_list[0].tab_name
+  metaTableKey.value++ // Trigger re-render if necessary
 }
 //获取数据
 const newDialog = (groupIndex: number, tabIndex: number) => {
-  resetData();
-  tShow.value = "新建分类";
-  bShow.value = false;
-  const detailIndex = 0;
-  editingIndex.value = { groupIndex, tabIndex, detailIndex };
-  currentRow.value.drawerVisible = true;
-};
+  resetData()
+  tShow.value = '新建站点'
+  bShow.value = false
+  const detailIndex = 0
+  editingIndex.value = { groupIndex, tabIndex, detailIndex }
+  currentRow.value.drawerVisible = true
+}
 //
 
 const updateNewSort = (details: any, groupIndex: number, tabIndex: number) => {
-  tableData.value[groupIndex].tab_list[tabIndex].details = details;
-  editingIndex.value = { groupIndex, tabIndex, detailIndex: 0 };
-  updateHeader();
-  metaTableKey.value++;
+  tableData.value[groupIndex].tab_list[tabIndex].details = details
+  editingIndex.value = { groupIndex, tabIndex, detailIndex: 0 }
+  updateHeader()
+  metaTableKey.value++
   //下面可以写对数据保存的一系列操作
-};
+}
 //点击保存数据按钮，对数据进行保存操作
 
 //新建的拉出抽屉
 const newSubmit = async (formEl: FormInstance | undefined) => {
-  if (!formEl) return;
+  if (!formEl) return
   try {
-    await formEl.validate();
+    await formEl.validate()
     const newItem = {
       title: formData.value.title,
       url: formData.value.url,
@@ -195,84 +236,82 @@ const newSubmit = async (formEl: FormInstance | undefined) => {
       description: formData.value.description,
       suffix: true,
       is_show: true,
-    };
-    if (editingIndex.value) {
-      const { groupIndex, tabIndex } = editingIndex.value;
-      tableData.value[groupIndex].tab_list[tabIndex].details.push(newItem);
     }
-    currentRow.value.drawerVisible = false;
-    await updateHeader();
-    openSuccessNew();
+    if (editingIndex.value) {
+      const { groupIndex, tabIndex } = editingIndex.value
+      tableData.value[groupIndex].tab_list[tabIndex].details.push(newItem)
+    }
+    currentRow.value.drawerVisible = false
+    await updateHeader()
+    openSuccessNew()
   } catch {
-    openWaring();
+    openWaring()
   }
-};
+}
 //新建的提交
 const showDialog = (row: any, groupIndex: number, tabIndex: number) => {
-  const detailIndex = getIndex(row, groupIndex, tabIndex);
-  bShow.value = true;
-  tShow.value = "修改分类";
-  editingIndex.value = { groupIndex, tabIndex, detailIndex };
-  Object.assign(currentRow.value, { ...row, drawerVisible: true });
+  const detailIndex = getIndex(row, groupIndex, tabIndex)
+  bShow.value = true
+  tShow.value = '修改站点'
+  editingIndex.value = { groupIndex, tabIndex, detailIndex }
+  Object.assign(currentRow.value, { ...row, drawerVisible: true })
   Object.assign(formData.value, {
     title: row.title,
     url: row.url,
     description: row.description,
     icon: row.icon,
-  });
-};
+  })
+}
 //给数据赋值，保证替换，抽屉的拖拽
 const onSubmit = async (formEl: FormInstance | undefined) => {
-  if (!formEl) return;
+  if (!formEl) return
   await formEl.validate(async (valid) => {
     if (valid && editingIndex.value) {
-      const { groupIndex, tabIndex, detailIndex } = editingIndex.value;
+      const { groupIndex, tabIndex, detailIndex } = editingIndex.value
       if (tableData.value) {
-        const detail = tableData.value[groupIndex].tab_list[tabIndex].details[detailIndex];
+        const detail =
+          tableData.value[groupIndex].tab_list[tabIndex].details[detailIndex]
         // 更新 detail 对象的属性
-        detail.title = formData.value.title;
-        detail.url = formData.value.url;
-        detail.description = formData.value.description;
-        detail.icon = formData.value.icon;
-        await updateHeader();
-        resetData();
-        currentRow.value.drawerVisible = false; // 关闭弹窗
-        openSuccessEdit();
+        detail.title = formData.value.title
+        detail.url = formData.value.url
+        detail.description = formData.value.description
+        detail.icon = formData.value.icon
+        await updateHeader()
+        resetData()
+        currentRow.value.drawerVisible = false // 关闭弹窗
+        openSuccessEdit()
       }
     } else {
-      openWaring();
+      openWaring()
     }
-  });
-};
+  })
+}
 const change = (groupIndex: number, tabIndex: number) => {
-  editingIndex.value = { groupIndex, tabIndex, detailIndex: 0 };
-  updateHeader();
-};
+  editingIndex.value = { groupIndex, tabIndex, detailIndex: 0 }
+  updateHeader()
+}
 //修改数据的提交
 const resetData = () => {
   formData.value = {
-    title: "",
-    url: "",
-    description: "",
-    icon: "",
-  };
-};
+    title: '',
+    url: '',
+    description: '',
+    icon: '',
+  }
+}
 //删除
 const deleteRow = (row: any, groupIndex: number, tabIndex: number) => {
-  const detailIndex = getIndex(row, groupIndex, tabIndex);
-  editingIndex.value = { groupIndex, tabIndex, detailIndex };
-  tableData.value[groupIndex].tab_list[tabIndex].details.splice(
-    detailIndex,
-    1
-  );
-  updateHeader();
-};
+  const detailIndex = getIndex(row, groupIndex, tabIndex)
+  editingIndex.value = { groupIndex, tabIndex, detailIndex }
+  tableData.value[groupIndex].tab_list[tabIndex].details.splice(detailIndex, 1)
+  updateHeader()
+}
 onMounted(async () => {
-  await download(); // 下载数据
+  await download() // 下载数据
   if (tableData.value.length > 0 && tableData.value[0].tab_list.length > 0) {
-    tabName.value = tableData.value[0].tab_list[0].tab_name;
+    tabName.value = tableData.value[0].tab_list[0].tab_name
   }
-});
+})
 </script>
 
 <style scoped>
